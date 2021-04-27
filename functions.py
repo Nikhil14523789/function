30b2eb57c4067c0914027aca4f542f8e0f82ee48
Nikhil14523789 <82226045+Nikhil14523789@users.noreply.github.com>
2021-04-27 23:56:22 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,70 @@
+##functions
+##def add():#2
+##    print("welcome to my world")#3
+##add()#1
+##def add(name):
+##    print(name,"welcome to my world")
+##add("nikhil")
+##add("saicharan")
+##add("karthik")
+##def add(name,age):
+##    print(age,"age is",name)
+##add("nihkil",21)
+##add("karthik",20)
+####add("charan",22)
+##def add(runs,name="nikhil",team="rcb"):#functon with defult arguments
+##    print(name,"scored",runs,"runs from",team)
+##add("100",team="csk")
+##add("183")
+
+##def add(runs,player,team):
+##    print(runs,'scored',team,'runs against',player)
+##add(runs="183*",player="virat",team="pakisthan")
+##add(183,"virat","pak")
+##add("rohi
+
+##def add(*name):
+##    print(name)
+##add("nikhil")
+##add("nikhil","sai","karthik","praneeth")
+##def add(**var):
+##    print(var)
+##add(name="virat")
+##add(name="dhoni",team="india",runs=65)
+
+##def name(name,marks,per):
+##    print(name,"welcome to my world",marks,per)
+##name("183","nikhil","75%")
+##name("puneeth",676,"90%")
+##name(marks=76,name="sai",per="74
+##def name(marks,name="sai",per="64%"):
+##    print(name,per,marks)
+##name(64)
+##counter=0
+##def fun():
+##    global counter
+##    for counter in range(1,101):
+##        print("welcome",counter)
+##fun()
+def add(a,b):
+    sum=a+b
+    return sum
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+res=add(a,b)
+print("addition of two number is",res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+          
